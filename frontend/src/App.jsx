27337800183b55c6a5888b9181a24a8f812b39d6
Ghaith_Hmaied <ghaithhm333@gmail.com>
2024-06-03 15:23:10 +0100

    Create and Link the pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,29 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Shop from './Pages/Shop'
+import ShopCategory from './Pages/ShopCategory'
+import Product from './Pages/Product'
+import Cart from './Pages/Cart'
+import LoginSignUp from './Pages/LoginSignUp'
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
        <Navbar/>
+       <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/men' element={<ShopCategory category="men"/>}/>
+          <Route path='/wemen' element={<ShopCategory category="women"/>}/>
+          <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+          <Route path='/product' element={<Product/>}/>
+          <Route path=':ProductId' element={<Product/>}/>
+       </Routes>
+       <Route path='/cart' element={<Cart/>}/>
+       <Route path='/login' element={<LoginSignUp/>}/>
+    </BrowserRouter>
     </>
   )
 }
