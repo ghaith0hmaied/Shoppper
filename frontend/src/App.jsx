b62e1bf39d89f2409f51aeff1f78c941d67180c0
Ghaith_Hmaied <ghaithhm333@gmail.com>
2024-06-04 17:54:58 +0100

    mapping items to the shop page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,11 +18,13 @@ function App() {
           <Route path='/men' element={<ShopCategory category="men"/>}/>
           <Route path='/wemen' element={<ShopCategory category="women"/>}/>
           <Route path='/kids' element={<ShopCategory category="kid"/>}/>
-          <Route path='/product' element={<Product/>}/>
-          <Route path=':ProductId' element={<Product/>}/>
+          <Route path='/product' element={<Product/>}>
+            <Route path=':ProductId' element={<Product/>}/>
+          </Route>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/login' element={<LoginSignUp/>}/>
+
        </Routes>
-       <Route path='/cart' element={<Cart/>}/>
-       <Route path='/login' element={<LoginSignUp/>}/>
     </BrowserRouter>
     </>
   )
