2872c6bff3c4d849c11fdf20759cf5acee94e8c4
Ghaith_Hmaied <ghaithhm333@gmail.com>
2024-06-11 16:25:25 +0100

    trying to fix product component

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,16 +1,21 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import { ShopContext } from '../Context/ShopContext';
-import { useParams } from 'next/navigation';
+import Breadcrum from '../Components/Breadcrum/Breadcrum';
+import { useParams } from 'react-router-dom';
 
 const Product = () => {
-    const {all_product} = useContext(ShopContext)
-    const {ProductId}= useParams();
-    const product = all_product.find((e)=>e.id === Number(ProductId));
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const { all_product } = useContext(ShopContext);
+    const { productId } = useParams();
+
+    // Check if all_product is defined before using it with the find method
+    const product = all_product && all_product.find((e) => e.id === Number(productId));
+
+    return (
+        <div>
+            {/* Pass product only if it's defined */}
+            <Breadcrum product={product} />
+        </div>
+    );
+};
 
 export default Product;
